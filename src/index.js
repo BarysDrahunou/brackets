c24c80460ea38e0fb141a99b03e53c7f1087dc6a
Barys Drahunou <barysdrahunou@gmail.com>
2020-09-17 08:32:39 +0300

    fix: implement more readable solution

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-    let openBracketsArray = []
-    let closedBracketsArray = [];
+    let openBrackets = []
+    let closeBrackets = [];
     bracketsConfig.forEach((x) => {
-        openBracketsArray.push(x[0]);
-        closedBracketsArray.push(x[1]);
+        openBrackets.push(x[0]);
+        closeBrackets.push(x[1]);
     })
-    let arrayForCheck = [];
-    for (let i of str) {
-        if (openBracketsArray.indexOf(i) !== -1) {
-            if (closedBracketsArray.indexOf(i) !== -1 && arrayForCheck[arrayForCheck.length - 1] === i) {
-                arrayForCheck.pop();
-            } else {
-                arrayForCheck.push(i);
-            }
-            continue
+    let result = str;
+    for (let i = 0; i < result.length - 1; i += 1) {
+        if (openBrackets.indexOf(result[i]) === closeBrackets.indexOf(result[i + 1])
+            && openBrackets.indexOf(result[i]) !== -1) {
+            result = result.slice(0, i)
+                .concat(result.slice(i + 2));
+            i-=2;
         }
-        if (arrayForCheck.length > 0 && arrayForCheck[arrayForCheck.length - 1] === openBracketsArray[closedBracketsArray.indexOf(i)]) {
-            arrayForCheck.pop();
-            continue;
-        }
-        return false;
     }
-    return arrayForCheck.length === 0;
+    return result.length === 0;
 }
